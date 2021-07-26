42f8cdc3b6b8481161218685894130feb8fcd0d9
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-07-26 12:48:09 +0900

    wip

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { RouteDefinition } from 'solid-app-router';
 import { lazy } from 'solid-js';
-import { ContributorsData } from './pages/Contributors.data';
+// import { ContributorsData } from './pages/Contributors.data';
 import { BenchmarkData } from './pages/Benchmarks.data';
 import { DocsData } from './pages/Docs.data';
-import { TutorialData } from './pages/Tutorial.data';
-import { ResourceData } from './pages/Resources.data';
-import { Redirect } from './components/Redirect';
+// import { TutorialData } from './pages/Tutorial.data';
+// import { ResourceData } from './pages/Resources.data';
+// import { Redirect } from './components/Redirect';
 
 export const routes: RouteDefinition[] = [
   {
@@ -37,37 +37,37 @@ export const routes: RouteDefinition[] = [
     ],
     data: DocsData,
   },
-  {
-    path: '/examples/:id',
-    component: lazy(() => import('./pages/Examples')),
-  },
-  {
-    path: '/examples',
-    component: Redirect('/examples/counter'),
-  },
-  {
-    path: '/tutorial/:id',
-    component: lazy(() => import('./pages/Tutorial')),
-    data: TutorialData,
-  },
-  {
-    path: '/tutorial',
-    component: Redirect('/tutorial/introduction_basics'),
-  },
-  {
-    path: '/contributors',
-    component: lazy(() => import('./pages/Contributors')),
-    data: ContributorsData,
-  },
-  {
-    path: '/resources',
-    component: lazy(() => import('./pages/Resources')),
-    data: ResourceData,
-  },
-  {
-    path: '/media',
-    component: lazy(() => import('./pages/Media')),
-  },
+  // {
+  //   path: '/examples/:id',
+  //   component: lazy(() => import('./pages/Examples')),
+  // },
+  // {
+  //   path: '/examples',
+  //   component: Redirect('/examples/counter'),
+  // },
+  // {
+  //   path: '/tutorial/:id',
+  //   component: lazy(() => import('./pages/Tutorial')),
+  //   data: TutorialData,
+  // },
+  // {
+  //   path: '/tutorial',
+  //   component: Redirect('/tutorial/introduction_basics'),
+  // },
+  // {
+  //   path: '/contributors',
+  //   component: lazy(() => import('./pages/Contributors')),
+  //   data: ContributorsData,
+  // },
+  // {
+  //   path: '/resources',
+  //   component: lazy(() => import('./pages/Resources')),
+  //   data: ResourceData,
+  // },
+  // {
+  //   path: '/media',
+  //   component: lazy(() => import('./pages/Media')),
+  // },
   {
     path: '*all',
     component: lazy(() => import('./pages/404')),
